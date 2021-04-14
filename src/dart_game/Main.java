7672057d5ab6f2f6f3428073d651ee9e3ffdf5d4
pms <ms147850@gmail.com>
2021-04-14 10:30:35 +0900

    Dart game

--- src/dart_game/Main.java
+++ src/dart_game/Main.java
@@ -2,75 +2,54 @@ package dart_game;
 
 public class Main {
 	public static void main(String[] args) {
-		String dartResult = "1D2S#10S";
-		
-		String regex = "[A-Z][*#]?";
-		String[] split = new String[3];
-		int cnt = 0;
-		
-		
-		StringBuffer sb = new StringBuffer();
-		sb.append(dartResult.charAt(0));
-		for (int i = 1; i < dartResult.length(); i++) {
-			if(('0'<=dartResult.charAt(i) && dartResult.charAt(i)<='9')){
-				split[cnt++] = sb.toString();
-				sb = new StringBuffer();
-			}
-			sb.append(dartResult.charAt(i));
-		}
-		System.out.println(split[0]);
-		System.out.println(split[1]);
-		System.out.println(split[2]);
-//		for (int i = 0; i < dartResult.length(); i++) {
-//			if(('A'<=dartResult.charAt(i) && dartResult.charAt(i)<='Z') && dartResult.charAt(i)!='#'){
-//				System.out.println(dartResult.substring(cnt, i+1));
-//				cnt=i+1;
-//			}
-//		}
-		
-		
-		/*
+		String dartResult = "1T2D3D#";
+//		String[] arrTemp = dartResult.split("");  //문자열을 String 배열에 각각 저장.
 		int[] arr = new int[3];
 		int arr_cnt = 0;
 		int num = 0;
-		for(int i=0; i<split.length;i++){
+		for(int i=0; i<dartResult.length();i++){
 			if('0' <= dartResult.charAt(i) && dartResult.charAt(i) <= '9'){
-				num = Integer.parseInt(dartResult.charAt(i)+"");
-				i++;
+				if(dartResult.charAt(i)=='1' && dartResult.charAt(i+1)=='0'){
+					num = 10;
+					i++;
+				}
+				else num = Integer.parseInt(dartResult.charAt(i)+"");
 			}
 			switch(dartResult.charAt(i)){
 				case 'S':
+					System.out.println("num_S : "+num);
 					arr[arr_cnt] = ((int) Math.pow(num, 1));
-					i++;
 					break;
 				case 'D':
+					System.out.println("num_D : " + num);
 					arr[arr_cnt] = ((int) Math.pow(num, 2));
-					i++;
 					break;
 				case 'T':
+					System.out.println("num_T : " + num);
 					arr[arr_cnt] = ((int) Math.pow(num, 3));
-					i++;
 					break;
 			}
-			if(i >=dartResult.length()) break;	
-			if(dartResult.charAt(i)=='*'){
-				if(arr_cnt==0){
-					
-					arr[arr_cnt] *=2;
-				}else{
-					arr[arr_cnt-1] *=2;
-					arr[arr_cnt] *=2;
+			if(i!=dartResult.length()-1){
+				if(dartResult.charAt(i+1)=='*'){
+					if(arr_cnt==0){
+						arr[arr_cnt] *=2;
+					}else{
+						arr[arr_cnt-1] *=2;
+						arr[arr_cnt] *=2;
+					}
 				}
-				i++;
+				else if(dartResult.charAt(i+1) == '#'){
+					arr[arr_cnt] *= -1;
+				}
 			}
-			else if(dartResult.charAt(i) == '#'){
-				arr[arr_cnt] *= -1;
-				i++;
-			}
-			arr_cnt++;
+			else if(i==dartResult.length()-1) break;
+//			System.out.println("arr_cnt : " + arr_cnt+", i : " + i);
+			
+			if(arr[arr_cnt]!=0)	arr_cnt++;
 			
 		}
-		*/
+		//1S*2T*3S
+		System.out.println(arr[0]+arr[1]+arr[2]);
 	}
 }
 
